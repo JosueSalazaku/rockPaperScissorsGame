086316c46a64c9c82356aaaf9c88b409012a8f41
Josue Sebastiao <josueseba@Josue-2.local>
2023-03-12 22:59:33 +0100

    app.js

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('reuslt')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice 
+let result
 
 possibleChoices.forEach(possibleChoices => possibleChoices.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML =  userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice() {
@@ -23,4 +25,29 @@ function generateComputerChoice() {
         computerChoice = 'scissors'
     }
     computerChoiceDIsplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'its a draw!' 
+    }
+    if (computerChoice === 'rock' && userChoice === "paper") {
+        result = 'Ayo you good at this boy!' 
+    }
+    if (computerChoice === 'rock' && userChoice === "scissors") {
+        result = 'You issa loser my boy!' 
+    }
+    if (computerChoice === 'paper' && userChoice === "scissors") {
+        result = 'you issa winner!' 
+    }
+    if (computerChoice === 'paper' && userChoice === "rock") {
+        result = 'You lose my boy!' 
+    }
+    if (computerChoice === 'scissors' && userChoice === "rock") {
+        result = 'You issa winner!' 
+    }
+    if (computerChoice === 'scissors' && userChoice === "paper") {
+        result = 'You issa loser my boy!' 
+    }
+    
 }
